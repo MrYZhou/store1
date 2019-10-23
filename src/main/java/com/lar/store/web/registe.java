3931a634534790f88398da07733b3ffc9131909e
22719 <176286175@qq.com>
2019-10-23 22:37:02 +0800

    a store demo

--- src/main/java/com/lar/store/web/registe.java
+++ src/main/java/com/lar/store/web/registe.java
@@ -1,5 +1,6 @@
 package com.lar.store.web;
 
+import com.lar.store.pojo.User;
 import com.lar.store.service.impl.RegisteServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +13,20 @@ public class registe {
     @Autowired
     RegisteServiceImpl registeServiceImpl;
     @RequestMapping(value = "/registePage",method = RequestMethod.GET)
-    public ModelAndView registe(){
+    public ModelAndView registePage(){
 
-        ModelAndView mv=new ModelAndView("registe");
-        return mv;
+        ModelAndView modelAndView=new ModelAndView("registe");
+        return modelAndView;
     }
     @RequestMapping(value = "/registe",method = RequestMethod.POST)
-    public String doregiste(){
-
-
+    public String doregiste(User user){
+        //
+        System.out.println(user);
+        return "";
+    }
+    @RequestMapping(value = "/checkAccount",method = RequestMethod.GET)
+    public String account_check(){
+        //
         return "";
     }
 }
