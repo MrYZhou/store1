d9a4fcb923715d073b8d020da1dc405e92c99a50
22719 <176286175@qq.com>
2019-10-24 00:07:20 +0800

    a store demo

--- src/main/java/com/lar/store/web/login.java
+++ src/main/java/com/lar/store/web/login.java
@@ -1,5 +1,8 @@
 package com.lar.store.web;
 
+import com.lar.store.pojo.User;
+import com.lar.store.service.impl.LoginServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,15 +10,24 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class login {
+    @Autowired
+    LoginServiceImpl loginService;
     @RequestMapping(value = "/loginPage",method = RequestMethod.GET)
     public ModelAndView login(){
         ModelAndView mv=new ModelAndView("login");
         return mv;
     }
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String dologin(){
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String dologin(User user){
         //进行登录
+        System.out.println(user);
+        if(user.getAccount()!=null){
+            String pwd=loginService.get_pwd(user.getAccount());
+            System.out.println("密码是"+pwd);
+        }else{
+            return "2";
+        }
 
-        return "";
+        return "1";
     }
 }
